b201d8b62e832a50f13180e0161686ef087b11b9
Adem Ait Fonollà <56362556+ademait@users.noreply.github.com>
2026-01-22 11:08:58 +0100

    Modified files give error in other file
    Committing to check if this is because of my changes

--- packages/editor/src/main/packages/swarm-diagram/agent-group/agent-group-update.tsx
+++ packages/editor/src/main/packages/swarm-diagram/agent-group/agent-group-update.tsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, ComponentClass } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { Textfield } from '../../../components/controls/textfield/textfield';
 import { Dropdown } from '../../../components/controls/dropdown/dropdown'; // For enums
+import { Button } from '../../../components/controls/button/button';
+import { TrashIcon } from '../../../components/controls/icon/trash';
+import { Divider } from '../../../components/controls/divider/divider';
+import { styled } from '../../../components/theme/styles';
 import { UMLElementRepository } from '../../../services/uml-element/uml-element-repository';
 import { AgentGroup, IAgentGroup } from './agent-group';
 import { ModelState } from '../../../components/store/model-state';
+
+const Flex = styled.div`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+`;
 
 // Define framework options
 const frameworkOptions = [
@@ -19,41 +30,64 @@ type StateProps = {};
 
 interface DispatchProps {
   update: typeof UMLElementRepository.update;
+  delete: typeof UMLElementRepository.delete;
 }
 
 type Props = OwnProps & StateProps & DispatchProps;
 
+const enhance = compose<ComponentClass<OwnProps>>(
+  connect<StateProps, DispatchProps, OwnProps, ModelState>(null, {
+    update: UMLElementRepository.update,
+    delete: UMLElementRepository.delete,
+  }),
+);
+
 class AgentGroupUpdateComponent extends Component<Props> {
-  private changeFramework = (id: string) => (framework: string) => {
-      this.props.update<IAgentGroup>(id, { framework });
+  private rename = (id: string) => (value: string) => {
+    this.props.update<IAgentGroup>(id, { name: value });
+  };
+
+  private changeFramework = (id: string) => (value: string) => {
+    this.props.update<IAgentGroup>(id, { framework: value });
+  };
+
+  private changeNumAgents = (id: string) => (value: string) => {
+    this.props.update<IAgentGroup>(id, { numAgents: parseInt(value, 10) || 1 });
+  };
+
+  private delete = (id: string) => () => {
+    this.props.delete(id);
   };
   
   render() {
     const { element } = this.props;
     return (
       <div>
-        <label>Name</label>
-        <Textfield
-          value={element.name}
-          onChange={(name) => this.props.update<IAgentGroup>(element.id, { name })}
-        />
-        <Dropdown
-          value={element.framework}
-          onChange={this.changeFramework(element.id)}>
+        <section>
+          <Flex>
+            <Textfield value={element.name} onChange={this.rename(element.id)} autoFocus />
+            <Button color="link" tabIndex={-1} onClick={this.delete(element.id)}>
+              <TrashIcon />
+            </Button>
+          </Flex>
+          <Divider />
+        </section>
+        <section>
+          <Dropdown value={element.framework} onChange={this.changeFramework(element.id)}>
             <Dropdown.Item value="BESSER-BAF">BESSER-BAF</Dropdown.Item>
-        </Dropdown>
-        <label>Number of Agents</label>
-        <Textfield
+          </Dropdown>
+        </section>
+        <section>
+          <Textfield
             value={String(element.numAgents)}
-            onChange={(value) => this.props.update<IAgentGroup>(element.id, { numAgents: parseInt(value, 10) || 1 })}
+            onChange={this.changeNumAgents(element.id)}
             type="number"
-        />
+            placeholder="Number of Agents"
+          />
+        </section>
       </div>
     );
   }
 }
 
-export const AgentGroupUpdate = connect<StateProps, DispatchProps, OwnProps, ModelState>(
-  null,
-  { update: UMLElementRepository.update }
-)(AgentGroupUpdateComponent);
+export const AgentGroupUpdate = enhance(AgentGroupUpdateComponent);
--- packages/editor/src/main/packages/swarm-diagram/swarm/swarm-update.tsx
+++ packages/editor/src/main/packages/swarm-diagram/swarm/swarm-update.tsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, ComponentClass } from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { Textfield } from '../../../components/controls/textfield/textfield';
 import { Dropdown } from '../../../components/controls/dropdown/dropdown'; // For enums
+import { Button } from '../../../components/controls/button/button';
+import { TrashIcon } from '../../../components/controls/icon/trash';
+import { Divider } from '../../../components/controls/divider/divider';
+import { styled } from '../../../components/theme/styles';
 import { UMLElementRepository } from '../../../services/uml-element/uml-element-repository';
 import { Swarm, ISwarm } from './swarm';
 import { ModelState } from '../../../components/store/model-state';
+
+const Flex = styled.div`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+`;
 
 interface OwnProps {
   element: Swarm;
@@ -14,34 +25,49 @@ type StateProps = {};
 
 interface DispatchProps {
   update: typeof UMLElementRepository.update;
+  delete: typeof UMLElementRepository.delete;
 }
 
 type Props = OwnProps & StateProps & DispatchProps;
+
+const enhance = compose<ComponentClass<OwnProps>>(
+  connect<StateProps, DispatchProps, OwnProps, ModelState>(null, {
+    update: UMLElementRepository.update,
+    delete: UMLElementRepository.delete,
+  }),
+);
 
 class SwarmUpdateComponent extends Component<Props> {
   private changeFramework = (id: string) => (framework: string) => {
     this.props.update<ISwarm>(id, { framework });
   };
+  private rename = (id: string) => (value: string) => {
+    this.props.update<ISwarm>(id, { name: value });
+  };
+  private delete = (id: string) => () => {
+    this.props.delete(id);
+  };
   render() {
     const { element } = this.props;
     return (
       <div>
-        <label>Name</label>
-        <Textfield
-          value={element.name}
-          onChange={(name) => this.props.update<ISwarm>(element.id, { name })}
-        />
-        <Dropdown
-          value={element.framework}
-          onChange={this.changeFramework(element.id)}>
+        <section>
+          <Flex>
+            <Textfield value={element.name} onChange={this.rename(element.id)} autoFocus />
+            <Button color="link" tabIndex={-1} onClick={this.delete(element.id)}>
+              <TrashIcon />
+            </Button>
+          </Flex>
+          <Divider />
+        </section>
+        <section>
+          <Dropdown value={element.framework} onChange={this.changeFramework(element.id)}>
             <Dropdown.Item value="BESSER-BAF">BESSER-BAF</Dropdown.Item>
-        </Dropdown>
+          </Dropdown>
+      </section>
       </div>
     );
   }
 }
 
-export const SwarmUpdate = connect<StateProps, DispatchProps, OwnProps, ModelState>(
-  null,
-  { update: UMLElementRepository.update }
-)(SwarmUpdateComponent);
+export const SwarmUpdate = enhance(SwarmUpdateComponent);
